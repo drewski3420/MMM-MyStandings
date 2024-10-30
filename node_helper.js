364a22e3e55026498851b4992b152d562715bd45
dathbe <github@beffa.us>
2024-10-30 07:24:06 -0700

    Update node_helper.js
    replace request with axios

--- node_helper.js
+++ node_helper.js
@@ -3,7 +3,7 @@
  *
  */
 var NodeHelper = require('node_helper');
-var request = require('request');
+//var request = require('request');
 const axios = require("axios");
 
 module.exports = NodeHelper.create({
@@ -14,17 +14,28 @@ module.exports = NodeHelper.create({
 	getData: function (notification, payload) {
 		var self = this;
 		//console.log('requesting:' + payload.url);
-		request({ url: payload.url, method: 'GET' }, function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var result = JSON.parse(body);
+		// request({ url: payload.url, method: 'GET' }, function (error, response, body) {
+		// 	if (!error && response.statusCode == 200) {
+		// 		var result = JSON.parse(body);
+		// 		self.sendSocketNotification(notification, {
+		// 			result: result,
+		// 			uniqueID: payload.uniqueID
+		// 		});
+		// 	} else {
+		// 		console.log("MMM-MyStandings : Could not load data.");
+		// 	}
+		// });
+		axios.get(payload.url)
+			.then( function(response) {
+				var result = JSON.parse(response.data);
 				self.sendSocketNotification(notification, {
 					result: result,
 					uniqueID: payload.uniqueID
 				});
-			} else {
-				console.log("MMM-MyStandings : Could not load data.");
-			}
-		});
+			})
+			.catch( function(r_err) {
+				console.log( "MMM-MyStandings : Could not load data." );      
+			})
 	},
 
 	//Subclass socketNotificationReceived received.
