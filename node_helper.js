e3ddd7f7ad025659a216116a3304d3e4297e8622
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2025-03-29 22:19:51 +0100

    Handle linting issues

--- node_helper.js
+++ node_helper.js
@@ -2,52 +2,53 @@
  * Module: Standings
  *
  */
-const Log = require("logger");
-const NodeHelper = require('node_helper');
-const dirTree = require("directory-tree");
+const Log = require('logger')
+const NodeHelper = require('node_helper')
+const dirTree = require('directory-tree')
 
 module.exports = NodeHelper.create({
-	start: function () {
-		Log.log('MMM-MyStandings helper started ...');
-		
-		this.localLogos = {};
-		const fsTree = dirTree("./modules/MMM-MyStandings/logos", {
-			extensions: /\.(svg|png)$/
-		});
-		fsTree.children.forEach( league => {
-			if (league.children) {
-				var logoFiles = [];
-				league.children.forEach( logo => {
-					logoFiles.push(logo.name);
-				});
-				this.localLogos[league.name] = logoFiles;
-			}
-		});
+  start: function () {
+    Log.log('MMM-MyStandings helper started ...')
 
-	},
+    this.localLogos = {}
+    const fsTree = dirTree('./modules/MMM-MyStandings/logos', {
+      extensions: /\.(svg|png)$/,
+    })
+    fsTree.children.forEach((league) => {
+      if (league.children) {
+        var logoFiles = []
+        league.children.forEach((logo) => {
+          logoFiles.push(logo.name)
+        })
+        this.localLogos[league.name] = logoFiles
+      }
+    })
+  },
 
-	async getData (notification, payload) {
-		try {
-			const response = await fetch(payload.url);
-			if (!response.ok) {
-				throw new Error(`HTTP error! status: ${response.status}`);
-			}
-			const data = await response.json();
-			this.sendSocketNotification(notification, {
-				result: data,
-				uniqueID: payload.uniqueID
-			});
-		} catch (error) {
-			Log.error("[MMM-MyStandings] Could not load data.", error);
-		}
-	},
+  async getData(notification, payload) {
+    try {
+      const response = await fetch(payload.url)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const data = await response.json()
+      this.sendSocketNotification(notification, {
+        result: data,
+        uniqueID: payload.uniqueID,
+      })
+    }
+    catch (error) {
+      Log.error('[MMM-MyStandings] Could not load data.', error)
+    }
+  },
 
-	//Subclass socketNotificationReceived received.
-	socketNotificationReceived: function(notification, payload) {
-		if (notification.startsWith("STANDINGS_RESULT")) {
-			this.getData(notification, payload);
-		} else if (notification == "MMM-MYSTANDINGS-GET-LOCAL-LOGOS") {
-			this.sendSocketNotification("MMM-MYSTANDINGS-LOCAL-LOGO-LIST", {instanceId: payload.instanceId, index: payload.index, logos: this.localLogos});
-	}
-	}
-});
+  // Subclass socketNotificationReceived received.
+  socketNotificationReceived: function (notification, payload) {
+    if (notification.startsWith('STANDINGS_RESULT')) {
+      this.getData(notification, payload)
+    }
+    else if (notification == 'MMM-MYSTANDINGS-GET-LOCAL-LOGOS') {
+      this.sendSocketNotification('MMM-MYSTANDINGS-LOCAL-LOGO-LIST', { instanceId: payload.instanceId, index: payload.index, logos: this.localLogos })
+    }
+  },
+})
